9524975af3ce4c5e1c7a18860a8e22d0484d7947
samuriapapa <yury.gitman@gmail.com>
2014-02-17 16:52:57 -0500

    Macbook Air, 4 Shapes Work But Crash

--- 4physicsBodys_Linked_By_RopeJoint/MyScene.m
+++ 4physicsBodys_Linked_By_RopeJoint/MyScene.m
@@ -7,16 +7,92 @@
 //
 
 
-
 #import "MyScene.h"
+
+@interface MyScene()
+@property SKSpriteNode* mySquare1;
+@property SKSpriteNode* mySquare2;
+@property SKSpriteNode* mySquare3;
+@property SKSpriteNode* mySquare4;
+@property SKSpriteNode* myShelf;
+@property SKPhysicsJoint* myRopeJoint;
+@property SKPhysicsJoint* myRopeJoint1;
+@property SKPhysicsJoint* myRopeJoint2;
+
+@end
 
 @implementation MyScene
 
--(id)initWithSize:(CGSize)size {    
+-(void) activateJointRope{
+    
+    _myRopeJoint = [SKPhysicsJointLimit jointWithBodyA:_mySquare1.physicsBody bodyB:_mySquare2.physicsBody anchorA:_mySquare1.position anchorB:_mySquare2.position];
+    
+    [self.physicsWorld addJoint:_myRopeJoint];
+    
+    _myRopeJoint1 = [SKPhysicsJointLimit jointWithBodyA:_mySquare2.physicsBody bodyB:_mySquare3.physicsBody anchorA:_mySquare2.position anchorB:_mySquare3.position];
+    
+    [self.physicsWorld addJoint:_myRopeJoint1];
+    
+    
+    _myRopeJoint2 = [SKPhysicsJointLimit jointWithBodyA:_mySquare3.physicsBody bodyB:_mySquare4.physicsBody anchorA:_mySquare3.position anchorB:_mySquare4.position];
+    
+    [self.physicsWorld addJoint:_myRopeJoint2];
+    
+}
+
+-(void) spawnSquares{
+    _mySquare1 = [[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(70, 70)];
+    _mySquare2 =[[SKSpriteNode alloc]initWithColor:[SKColor purpleColor] size:CGSizeMake(70, 70)];
+    _mySquare3 =[[SKSpriteNode alloc]initWithColor:[SKColor orangeColor] size:CGSizeMake(70, 70)];
+    _mySquare4 =[[SKSpriteNode alloc]initWithColor:[SKColor yellowColor] size:CGSizeMake(70, 70)];
+    
+    
+    [_mySquare1 setPosition:CGPointMake(self.size.width/1.5, self.size.height/1.5)];
+    [_mySquare2 setPosition:CGPointMake(self.size.width/1.5, self.size.height/2)];
+    [_mySquare3 setPosition:CGPointMake(self.size.width/1.5, self.size.height/2.5)];
+    [_mySquare4 setPosition:CGPointMake(self.size.width/1.5, self.size.height/3)];
+    
+    _mySquare1.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare1.size];
+    _mySquare2.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare2.size];
+    _mySquare3.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare3.size];
+    _mySquare4.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare4.size];
+    
+    [_mySquare1.physicsBody setRestitution:1.0];
+    [_mySquare2.physicsBody setRestitution:1.0];
+    [_mySquare3.physicsBody setRestitution:1.0];
+    [_mySquare4.physicsBody setRestitution:1.0];
+    
+    [self addChild:_mySquare1];
+    [self addChild:_mySquare2];
+    [self addChild:_mySquare3];
+    [self addChild:_mySquare4];
+    
+}
+
+-(void)makeShelf{
+    _myShelf = [[SKSpriteNode alloc]initWithColor:[SKColor lightGrayColor] size:CGSizeMake(100, 40)];
+    _myShelf.position = CGPointMake(self.size.width/2.4, self.size.height/2);
+    _myShelf.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_myShelf.size];
+    [_myShelf.physicsBody setDynamic:NO];
+    
+    [self addChild:_myShelf];
+    
+}
+
+-(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
-
+        self.scaleMode = SKSceneScaleModeAspectFit;
+        self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+        [self.physicsBody setRestitution:1];
+        
+        
+        [self spawnSquares];
+        
+        [self activateJointRope];
+        [self makeShelf];
+        
     }
     return self;
 }
@@ -24,20 +100,50 @@
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     
+    if (_mySquare1.physicsBody.dynamic) {
+        
+        [_mySquare1.physicsBody setDynamic:NO];
+    }
+    
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
+        [_mySquare1 setPosition:location];
         
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
         
-        sprite.position = location;
+        // [_mySquare1.physicsBody setDynamic:NO];
+        //  [_mySquare2.physicsBody setDynamic:NO];
         
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
     }
 }
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    for (UITouch *touch in touches) {
+        CGPoint location = [touch locationInNode:self];
+        [_mySquare1 setPosition:location];
+        
+    }
+}
+
+-(void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event  {
+    
+    if (!_mySquare1.physicsBody.dynamic) {
+        [_mySquare1.physicsBody setDynamic:YES];
+    }
+    //  [_mySquare1.physicsBody setDynamic:YES];
+    //  [_mySquare2.physicsBody setDynamic:YES];
+    
+}
+
+-(void) touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event{
+    
+    if (!_mySquare1.physicsBody.dynamic) {
+        [_mySquare1.physicsBody setDynamic:YES];
+    }
+    //  [_mySquare1.physicsBody setDynamic:YES];
+    //  [_mySquare2.physicsBody setDynamic:YES];
+    
+}
+
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */

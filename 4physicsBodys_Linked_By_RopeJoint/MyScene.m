a61f332dae21cca3f098d10adbd1e4faeb0f6851
samuraipapa <yury.gitman@gmail.com>
2014-02-19 11:51:21 -0500

    8 Physics Bodies, Kinda Cool, Not Connected Buy Joints Yet Also Added Just Portrait Mode

--- 4physicsBodys_Linked_By_RopeJoint/MyScene.m
+++ 4physicsBodys_Linked_By_RopeJoint/MyScene.m
@@ -14,10 +14,17 @@
 @property SKSpriteNode* mySquare2;
 @property SKSpriteNode* mySquare3;
 @property SKSpriteNode* mySquare4;
+@property SKSpriteNode* mySquare5;
+@property SKSpriteNode* mySquare6;
+@property SKSpriteNode* mySquare7;
+@property SKSpriteNode* mySquare8;
 @property SKSpriteNode* myShelf;
 @property SKPhysicsJoint* myRopeJoint;
 @property SKPhysicsJoint* myRopeJoint1;
 @property SKPhysicsJoint* myRopeJoint2;
+@property SKPhysicsJoint* myRopeJoint3;
+@property SKPhysicsJoint* myRopeJoint4;
+@property SKPhysicsJoint* myRopeJoint5;
 
 @end
 
@@ -42,30 +49,51 @@
 
 -(void) spawnSquares{
     _mySquare1 = [[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(70, 70)];
-    _mySquare2 =[[SKSpriteNode alloc]initWithColor:[SKColor purpleColor] size:CGSizeMake(70, 70)];
-    _mySquare3 =[[SKSpriteNode alloc]initWithColor:[SKColor orangeColor] size:CGSizeMake(70, 70)];
-    _mySquare4 =[[SKSpriteNode alloc]initWithColor:[SKColor yellowColor] size:CGSizeMake(70, 70)];
-    
+    _mySquare2 =[[SKSpriteNode alloc]initWithColor:[SKColor purpleColor] size:CGSizeMake(60, 60)];
+    _mySquare3 =[[SKSpriteNode alloc]initWithColor:[SKColor orangeColor] size:CGSizeMake(50, 50)];
+    _mySquare4 =[[SKSpriteNode alloc]initWithColor:[SKColor yellowColor] size:CGSizeMake(40, 40)];
+    _mySquare5 = [[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(30, 30)];
+    _mySquare6 =[[SKSpriteNode alloc]initWithColor:[SKColor purpleColor] size:CGSizeMake(20, 20)];
+    _mySquare7 =[[SKSpriteNode alloc]initWithColor:[SKColor orangeColor] size:CGSizeMake(10, 10)];
+    _mySquare8 =[[SKSpriteNode alloc]initWithColor:[SKColor yellowColor] size:CGSizeMake(5, 5)];
     
     [_mySquare1 setPosition:CGPointMake(self.size.width/1.5, self.size.height/1.5)];
     [_mySquare2 setPosition:CGPointMake(self.size.width/1.5, self.size.height/2)];
     [_mySquare3 setPosition:CGPointMake(self.size.width/1.5, self.size.height/2.5)];
     [_mySquare4 setPosition:CGPointMake(self.size.width/1.5, self.size.height/3)];
+    [_mySquare5 setPosition:CGPointMake(self.size.width/1.5, self.size.height/1.5+5)];
+    [_mySquare6 setPosition:CGPointMake(self.size.width/1.5, self.size.height/2+5)];
+    [_mySquare7 setPosition:CGPointMake(self.size.width/1.5, self.size.height/2.5+5)];
+    [_mySquare8 setPosition:CGPointMake(self.size.width/1.5, self.size.height/3+5)];
+    
     
     _mySquare1.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare1.size];
     _mySquare2.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare2.size];
     _mySquare3.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare3.size];
     _mySquare4.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare4.size];
+    _mySquare5.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare5.size];
+    _mySquare6.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare6.size];
+    _mySquare7.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare7.size];
+    _mySquare8.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare8.size];
+
     
     [_mySquare1.physicsBody setRestitution:1.0];
     [_mySquare2.physicsBody setRestitution:1.0];
     [_mySquare3.physicsBody setRestitution:1.0];
     [_mySquare4.physicsBody setRestitution:1.0];
+    [_mySquare5.physicsBody setRestitution:1.0];
+    [_mySquare6.physicsBody setRestitution:1.0];
+    [_mySquare7.physicsBody setRestitution:1.0];
+    [_mySquare8.physicsBody setRestitution:1.0];
     
     [self addChild:_mySquare1];
     [self addChild:_mySquare2];
     [self addChild:_mySquare3];
     [self addChild:_mySquare4];
+    [self addChild:_mySquare5];
+    [self addChild:_mySquare6];
+    [self addChild:_mySquare7];
+    [self addChild:_mySquare8];
     
 }
 
